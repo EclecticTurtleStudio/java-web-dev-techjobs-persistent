270e179942893bbd0efb17009e9e19a1c67c25f9
Becky Jo Roth <eclecticturtlestudio@gmail.com>
2020-07-25 16:54:08 -0500

    Routing errors or something... Gonna push it before I continue on.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,6 +1,9 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -8,12 +11,16 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    EmployerRepository employerRepository;
 
     @RequestMapping("")
     public String index(Model model) {
@@ -25,6 +32,7 @@ public class HomeController {
 
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
+        model.addAttribute("employers", employerRepository);
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
         return "add";
@@ -37,9 +45,11 @@ public class HomeController {
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
             return "add";
+        } else {
+            Optional selectedEmployer = employerRepository.findById(employerId);
+            return "redirect:";
         }
 
-        return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,11 +1,17 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany(mappedBy = "employer")
+    private final List<Job> jobs = new ArrayList<>();
 
     @NotBlank
     @Size(max = 85, message = "Location may be no more than 85 characters")
@@ -25,4 +31,7 @@ public class Employer extends AbstractEntity {
         this.location = location;
     }
 
+    public List<Job> getJobs() {
+        return jobs;
+    }
 }
